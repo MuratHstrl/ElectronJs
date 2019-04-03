0cab315fc01ac7922ce62ffc1c878f539c9fc203
Murat Hastarla <38501366+MuratHstrl@users.noreply.github.com>
2019-04-03 14:26:55 +0300

    Electron.JS Giriş
    +Notlar yeri eklendi
    +html Dosyası Eklendi
    +Front end'den Back end'e veri gönderme eklendi

--- Elektron.js/main.js
+++ Elektron.js/main.js
@@ -1,41 +1,9 @@
-/*
-İmport Etmek İçin 1 numaralı Yerler Kullanılır.
-+
-+
-2 Numarada electron modülümüzden istediğimiz değişkeni çağırmak için kullanılır.
-+
-+
-3 numaralı yerde Kullanılacak ana ekran.
-+
-+
-4 numaralı yerde ise 2 Numarada elektron modülünden çağırdığımız App dosyası Hazırsa şunları yap.
-+
-+
-4.1 de ise pencere açma işlemlerini gerçekleştiriyoruz "pathname deki index i değiştirerek kendi html dosyanın ismini yazarak çalıştırabilirsin".
-+
-+
-4.2 de ise Kendi istediğimiz menüyü oluşturmak için kullanıyoruz
-+
-+
-5 Numarada Kendi Menümüzü Düzenleyebilir Submenu kullanarak dropdown oluşturabiliriz. 5numaralı yer Her bir süslü parantez "{}" 1 menuyü belirtir yan yana virgül atarak yazılırsa birden fazla menu butonunuz olur.
-+
-+
-Roller:
-quit = Çıkışı Sağlar.
-TODO = Tıklanabilir bir label oluşturmayı Sağlar.
-+
-+
-5.1 Numara Kısa Yol Eklememizi sağlar "darvin" Mac bilgisayarlar içi yazılmıştır
-+
-+
- 
-*/ 
 
 const electron = require("electron"); // 1
 const url = require("url"); // 1
 const path = require("path"); // 1
 
-const { app , BrowserWindow , Menu} = electron; // 2 
+const { app , BrowserWindow , Menu , ipcMain} = electron; // 2 
 
 let mainWindows; // 3 
 
@@ -49,8 +17,19 @@ mainWindows.loadURL(
        slashes : true
    })
 );
+
 const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);//4.2
-Menu.setApplicationMenu(mainMenu) // 4.2
+Menu.setApplicationMenu(mainMenu); // 4.2
+
+
+ipcMain.on("key" , (err , data) => {
+    console.log(data)
+});//4.3
+ipcMain.on("key:gelentxt", (err,data) => {
+    console.log(data)
+})//4.3
+
+
 }); // 4
 const mainMenuTemplate = [
     {
