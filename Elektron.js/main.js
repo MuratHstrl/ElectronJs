cd660b4d57a375849362a5d87882d08acd3b6fb4
Murat Hastarla <38501366+HoupSS@users.noreply.github.com>
2019-04-03 12:04:21 +0300

    Elektron Js
    Giriş 
    Menu Ekleme Eklendi

--- Elektron.js/main.js
+++ Elektron.js/main.js
@@ -9,13 +9,33 @@
 +
 +
 4 numaralı yerde ise 2 Numarada elektron modülünden çağırdığımız App dosyası Hazırsa şunları yap.
++
++
+4.1 de ise pencere açma işlemlerini gerçekleştiriyoruz "pathname deki index i değiştirerek kendi html dosyanın ismini yazarak çalıştırabilirsin".
++
++
+4.2 de ise Kendi istediğimiz menüyü oluşturmak için kullanıyoruz
++
++
+5 Numarada Kendi Menümüzü Düzenleyebilir Submenu kullanarak dropdown oluşturabiliriz. 5numaralı yer Her bir süslü parantez "{}" 1 menuyü belirtir yan yana virgül atarak yazılırsa birden fazla menu butonunuz olur.
++
++
+Roller:
+quit = Çıkışı Sağlar.
+TODO = Tıklanabilir bir label oluşturmayı Sağlar.
++
++
+5.1 Numara Kısa Yol Eklememizi sağlar "darvin" Mac bilgisayarlar içi yazılmıştır
++
++
+ 
 */ 
 
 const electron = require("electron"); // 1
 const url = require("url"); // 1
 const path = require("path"); // 1
 
-const { app , BrowserWindow } = electron; // 2 
+const { app , BrowserWindow , Menu} = electron; // 2 
 
 let mainWindows; // 3 
 
@@ -28,5 +48,26 @@ mainWindows.loadURL(
        protocol : "file:",
        slashes : true
    })
-)
-}) // 4
+);
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);//4.2
+Menu.setApplicationMenu(mainMenu) // 4.2
+}); // 4
+const mainMenuTemplate = [
+    {
+        label : "Dosya",
+        submenu : [
+            {
+                label: "Ayarlar"
+            },
+            {
+                label : "Çıkış",
+                accelerator: process.platform == "darwin" ? "Command + q" : "Ctrl + q", // 5.1
+                role : "quit" 
+            }
+        ]
+    },
+    {
+        label : "Deneme2"
+    }
+
+] // 5
